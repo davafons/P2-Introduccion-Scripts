ba159738398c0090d45614091dc63281bad3e7a5
David Afonso <davafons@gmail.com>
2019-10-11 13:18:03 +0100

    Add files via upload

---
+++ PlayerSquareMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSquareMovement : MonoBehaviour
+{
+    public float speed;
+    public float turn_speed;
+
+    public float slow_ratio = 0.5f;
+
+    void Start()
+    {
+        speed = 0.2f;
+        turn_speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float turnHorizontal = Input.GetAxis("Turn");
+
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+        Vector3 turnDirection = new Vector3(0, turnHorizontal, 0);
+
+        transform.Translate(moveDirection * speed);
+        transform.Rotate(turnDirection * turn_speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "BallObstacle") {
+            print("Enter Trigger " + other.tag);
+
+            speed *= slow_ratio;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "BallObstacle") {
+            print("Exit Trigger " + other.tag);
+
+            speed /= slow_ratio;
+        }
+    }
+}
